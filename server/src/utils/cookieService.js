369c2d43cc52576f6e38e4876db0da14772b9e6b
varunsinghal <varunsinghal3655@>
2024-09-01 23:48:28 +0530

    minor change

--- server/src/utils/cookieService.js
+++ server/src/utils/cookieService.js
@@ -2,7 +2,7 @@ export const setAuthCookie = (res, token) => {
     res.cookie(process.env.AUTH_COOKIE_NAME, token, {
         maxAge: 900000,
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: 'Strict',
     });
 };
