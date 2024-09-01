c58d9f256a1071bcd92f7beae85b8002af57407f
varunsinghal <varunsinghal3655@>
2024-09-01 22:22:26 +0530

    database is migrate to supabase as service

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { useAuth } from '../contexts/auth.context'
-import { Avatar } from 'evergreen-ui'
 
 export default function Home() {
 
     const { user } = useAuth()
     return (
-        <Avatar name={user.name} size={40} />
+        <p>Hie</p>
     )
 }
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,19 +1,18 @@
 // src/db.js
 import pkg from 'pg';
 const { Pool } = pkg;
-// Debugging: Log environment variables to check if they're correctly loaded
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '****' : 'Not set'); // Masking password for security
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PORT:', process.env.DB_PORT);
+// // Debugging: Log environment variables to check if they're correctly loaded
+// console.log('DB_HOST:', process.env.DB_HOST);
+// console.log('DB_USER:', process.env.DB_USER);
+// console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '****' : 'Not set'); // Masking password for security
+// console.log('DB_NAME:', process.env.DB_NAME);
+// console.log('DB_PORT:', process.env.DB_PORT);
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DB_URI,
+    ssl: {
+        rejectUnauthorized: false, // Important for Supabase connection
+    }
 });
 
 pool.on('error', (err, client) => {
